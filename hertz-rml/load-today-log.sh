82d9cf8adf136ab78943506806deab0676f50261
Keywan Najafi Tonekaboni <knajafit@TechFak.Uni-Bielefeld.DE>
2014-08-08 19:01:11 +0200

    changed from marker search to time based search

--- hertz-rml/load-today-log.sh
+++ hertz-rml/load-today-log.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# This script tries to load the log (aka playlist) for today and start
-# the log afterwards. If a marker name is suplied, it tries to start
-# jump to a marker with this label, otherwise it starts with the third 
-# item (Line 2) to avoid events at the beginning of the list, like scheduled
-# shows.
+# This script tries to load the log (aka playlist) for today and start the log
+# afterwards. If a start time is suplied, it tries to jump to the element in
+# the playlist at this time, otherwise it starts with the third item (Line 2)
+# to avoid events at the beginning of the list, like scheduled shows.
 
 # expects MySQL credentials in ~/.my.cnf
 
 
 # FAILSAFE load line 2, if we didn't have or find the marker
 LOG_LINE=2
-# TODO: get line 
+# TODO: get line
 
 # get the date in the form Rivendell use it: 2014_08_31
 NOW_DATE=$(date +%Y_%m_%d)
@@ -20,17 +19,21 @@ NOW_DATE=$(date +%Y_%m_%d)
 if [ $# -eq 1 ]; then
 
     # Label name of the marker from command line
-    MARKER_LABEL=$1
-    
+    #MARKER_LABEL=$1
+    START_TIME=$1
+
     # In which row we should lookup the number (ID or COUNT)?
     ROW="COUNT"
-    
+
+    TIME_MS=$(echo $START_TIME  | awk -F: '{ print ($1 * 3600 * 1000) + ($2 * 60 * 1000) }')
+
     # SQL Code to search for a line with the specified label
-    SQL='SELECT '$ROW' FROM '${NOW_DATE}'_LOG L WHERE LABEL LIKE "%'${MARKER_LABEL}'%"'
-    
-    # only take the first found line number. 
+    #SQL='SELECT '$ROW' FROM '${NOW_DATE}'_LOG L WHERE LABEL LIKE "%'${MARKER_LABEL}'%"'
+    SQL='SELECT '$ROW' FROM '${NOW_DATE}'_LOG L WHERE START_TIME LIKE '${TIME_MS}
+
+    # only take the first found line number.
     ID=$(echo $SQL | mysql -h localhost Rivendell | grep  '^[0-9]\+$' | head -n 1)
-    
+
     # Test expression for positive integer numbers
     REGEXP_NUMBER='^[0-9]+$'
 
@@ -40,7 +43,7 @@ if [ $# -eq 1 ]; then
     else
         echo "error:[${ID}] Not a number" >&2; exit 1
     fi
-    
+
 elif [ $# -gt 1 ]; then
     # Error output, if more then one argument is given at start time
     echo -e "\n\terror: max. one argument (marker label allowed)" >&2
@@ -60,7 +63,7 @@ rmlsend "LL 1 $NOW_DATE $LOG_LINE"!
 sleep 1
 
 # MN <mach> <line>!
-# TODO: Check: Necessary to set the next event? 
+# TODO: Check: Necessary to set the next event?
 # rmlsend "MN 1 $LOG_LINE"!
 
 # TODO: Check: WRONG: we dont want to use PL
