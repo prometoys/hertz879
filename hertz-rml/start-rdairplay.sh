e435fcde479022d143448e63159e5ed08a69df0a
Keywan Tonekaboni <me@prometoys.net>
2015-05-15 16:42:42 +0200

    on air flag, no screen saver in gnome

--- hertz-rml/start-rdairplay.sh
+++ hertz-rml/start-rdairplay.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-set +x
 
+# Start RDAirplay
 rdairplay &
 
+# Wait until RDAirplay is probably ready
 sleep 2
 
+# Activate ON AIR Flag
+rmlsend "TA 1"!
+
+# Load the log of today at now position.
 /usr/local/bin/load-today-log.sh `date +%H:%M`
 
+# Disable gnome screen blanking, if needed.
 gsettings set org.gnome.settings-daemon.plugins.power sleep-display-ac 0
 gsettings set org.gnome.desktop.session idle-delay 0
